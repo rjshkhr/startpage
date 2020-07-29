6f3a11f7d0c9b290f803152aab86cd1b6c082984
rajshekhar26 <raj.shekhar2622@gmail.com>
2020-07-29 17:14:07 +0530

    added userChrome.css

---
+++ newtab/local-settings.js
@@ -0,0 +1,4 @@
+pref("general.config.filename", "all-gentoo.js");
+pref("general.config.obscure_value", 0); // use this to disable the byte-shift
+pref("general.config.filename", "mozilla.cfg");
+pref("general.config.sandbox_enabled", false);
