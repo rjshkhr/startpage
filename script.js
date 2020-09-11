7eb7ad3ff2ef5bb5359f6c08937adba856c600a8
rajshekhar26 <raj.shekhar2622@gmail.com>
2020-09-11 19:02:18 +0530

    fix

--- script.js
+++ script.js
@@ -4,21 +4,20 @@ let body = document.body;
 let theme = localStorage.getItem('theme');
 let toggleTheme = document.getElementById('toggleTheme');
 
-toggleTheme.addEventListener('click', function() {
-    if (body.classList.contains('light')) {
-        body.classList.remove('light');
-        toggleTheme.textContent = 'Dark';
-        localStorage.setItem('theme', 'dark');
-    } else {
-        body.classList.add('light');
-        toggleTheme.textContent = 'Light';
-        localStorage.setItem('theme', 'light');
-  }
+toggleTheme.addEventListener('click', function () {
+	if (body.classList.contains('light')) {
+		body.classList.remove('light');
+		toggleTheme.textContent = 'Dark';
+		localStorage.setItem('theme', 'dark');
+	} else {
+		body.classList.add('light');
+		toggleTheme.textContent = 'Light';
+		localStorage.setItem('theme', 'light');
+	}
 });
 
 function getTheme() {
-
-    body.classList.add(theme)
-    toggleTheme.textContent = theme;
+	body.classList.add(theme);
+	toggleTheme.textContent = theme;
 }
 getTheme();
