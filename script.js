f6db086bcdb4dbcf394b594da956ee613953edc3
rajshekhar26 <rjshekhar.official@gmail.com>
2021-10-06 13:12:04 +0530

    Refactor

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 const body = document.body;
-const themeBtn = document.querySelector('.fas');
-const getBodyTheme = localStorage.getItem('theme');
-const getBtnTheme = localStorage.getItem('btn-theme');
+const themeBtn = document.querySelector(".fas");
+const getBodyTheme = localStorage.getItem("theme");
+const getBtnTheme = localStorage.getItem("btn-theme");
 
-const isLight = () => body.classList.contains('body-light');
+const isLight = () => body.classList.contains("body-light");
 
-const setTheme = (bodyClass, buttonClass) => {
-	document.body.classList.remove(localStorage.getItem('theme'));
-	themeBtn.classList.remove(localStorage.getItem('btn-theme'));
-	document.body.classList.add(bodyClass);
-	themeBtn.classList.add(buttonClass);
-	localStorage.setItem('theme', bodyClass);
-	localStorage.setItem('btn-theme', buttonClass);
+const addThemeClass = (bodyClass, btnClass) => {
+  body.classList.add(bodyClass);
+  themeBtn.classList.add(btnClass);
+};
+
+addThemeClass(getBodyTheme, getBtnTheme);
+
+const setTheme = (bodyClass, btnClass) => {
+  body.classList.remove(localStorage.getItem("theme"));
+  themeBtn.classList.remove(localStorage.getItem("btn-theme"));
+  addThemeClass(bodyClass, btnClass);
+  localStorage.setItem("theme", bodyClass);
+  localStorage.setItem("btn-theme", btnClass);
 };
 
 const toggleTheme = () =>
-	isLight()
-		? setTheme('body-dark', 'fa-sun')
-		: setTheme('body-light', 'fa-moon');
+  isLight()
+    ? setTheme("body-dark", "fa-sun")
+    : setTheme("body-light", "fa-moon");
 
-themeBtn.addEventListener('click', toggleTheme);
-
-const setBodyTheme = () => body.classList.add(getBodyTheme);
-const setBtnTheme = () => themeBtn.classList.add(getBtnTheme);
-
-setBodyTheme();
-setBtnTheme();
+themeBtn.addEventListener("click", toggleTheme);
